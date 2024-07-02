28aa9a1a718e648ee6f9bf406afee9ea01318965
Tom Buckley <tbuckley89@gmail.com>
2024-07-01 21:42:39 -0400

    Add streaming

--- llm_lm_studio.py
+++ llm_lm_studio.py
@@ -7,16 +7,21 @@ def register_models(register):
 
 class Markov(llm.Model):
     model_id = "lm-studio"
+    can_stream = True
 
     def execute(self, prompt, stream, response, conversation):
         client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-        completion = client.chat.completions.create(
+        response = client.chat.completions.create(
             model="bartowski/gemma-2-9b-it-GGUF/gemma-2-9b-it-Q4_K_M.gguf",
             messages=[
                 *([{"role": "system", "content": prompt.system}] if prompt.system is not None else []),
                 {"role": "user", "content": prompt.prompt},
             ],
             temperature=0.7,
+            stream=True,
         )
 
-        return [choice.message.content for choice in completion.choices]
+        for chunk in response:
+            content = chunk.choices[0].delta.content
+            if content is not None:
+                yield content
