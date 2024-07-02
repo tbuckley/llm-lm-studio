9d58735a62bdb1e16f15183bc4a61dd7d3aa8a1e
Tom Buckley <tbuckley89@gmail.com>
2024-07-01 21:33:42 -0400

    Now appears to work

--- llm_lm_studio.py
+++ llm_lm_studio.py
@@ -1,4 +1,5 @@
 import llm
+from openai import OpenAI
 
 @llm.hookimpl
 def register_models(register):
@@ -8,4 +9,14 @@ class Markov(llm.Model):
     model_id = "lm-studio"
 
     def execute(self, prompt, stream, response, conversation):
-        return ["hello world"]
+        client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+        completion = client.chat.completions.create(
+            model="bartowski/gemma-2-9b-it-GGUF/gemma-2-9b-it-Q4_K_M.gguf",
+            messages=[
+                *([{"role": "system", "content": prompt.system}] if prompt.system is not None else []),
+                {"role": "user", "content": prompt.prompt},
+            ],
+            temperature=0.7,
+        )
+
+        return [choice.message.content for choice in completion.choices]
